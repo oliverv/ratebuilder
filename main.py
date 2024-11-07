75290cfa8f637d5a49bd3d95ec2fbbf4df9d9585
Olivier Vermeulen <oliver@oliverv.com>
2024-11-07 01:22:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import streamlit as st
 import subprocess
 from PIL import Image
 
-
-
-# Display a menu to select which app to run
+# Display logo and menu title
 logo = Image.open("logo.png")
 st.image(logo, width=50)
-
 st.title("Application Menu")
 
-option = st.selectbox("Choose an application to launch:", ("telecall_rate_builder", "checker" , "upload" , "streamlit_app"))
+# Application options
+options = {
+    "telecall_rate_builder": "telecall_rate_builder.py",
+    "checker": "check.py",
+    "upload": "upload.py",
+    "streamlit_app": "streamlit_app.py",
+    "telecall_rate_builder_old": "telecall_rate_builder_old.py"
+}
+
+# Select application from dropdown
+option = st.selectbox("Choose an application to launch:", list(options.keys()))
 
 # Run the selected application
 if st.button("Launch Application"):
-    if option == "telecall_rate_builder":
-        subprocess.Popen(["streamlit", "run", "telecall_rate_builder.py", "--server.port=8081"])
-    if option == "checker":
-        subprocess.Popen(["streamlit", "run", "check.py", "--server.port=8081"])
+    selected_app = options[option]
+    subprocess.Popen(["streamlit", "run", selected_app, "--server.port=8081"])
     st.write(f"Launching {option}... Refresh the page to reset.")
-    if option == "upload":
-        subprocess.Popen(["streamlit", "run", "upload.py", "--server.port=8081"])
-    st.write(f"Launching {option}... Refresh the page to reset.")
-    if option == "streamlit_app":
-        subprocess.Popen(["streamlit", "run", "streamlit_app.py", "--server.port=8081"])
-    st.write(f"Launching {option}... Refresh the page to reset.")
-    elif option == "telecall_rate_builder_old":
-        subprocess.Popen(["streamlit", "run", "telecall_rate_builder_old.py", "--server.port=8081"])
-    st.write(f"Launching {option}... Refresh the page to reset.")
-    
